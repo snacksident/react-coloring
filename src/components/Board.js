be01572f6d60e82ea6100d5d8c7a2596496e4c16
snacksident <leak187@gmail.com>
2022-04-28 10:56:08 -0700

    extremely basic setup

---
+++ src/components/Board.js
@@ -0,0 +1,19 @@
+import Space from "./Space"
+
+export default function Board() {
+    //state likely held here
+    return(
+        <>
+        <h1>this is the board</h1>
+        <Space />
+        <Space />
+        <Space />
+        <Space />
+        <Space />
+        <Space />
+        <Space />
+        <Space />
+        <Space />
+        </>
+    )
+}
---
+++ src/components/Space.js
@@ -0,0 +1,7 @@
+export default function Space(){
+    return(
+        <div className="space-div">
+            this is a space in a div
+        </div>
+    )
+}
