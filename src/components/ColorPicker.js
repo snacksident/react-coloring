9c8cd93cf1be7826b7fcf723f1f288c583d16a5e
snacksident <leak187@gmail.com>
2022-04-30 11:01:39 -0700

    change colorpicker to have neon colors

--- src/components/ColorPicker.js
+++ src/components/ColorPicker.js
@@ -1,8 +1,6 @@
 import { CirclePicker } from 'react-color'
 
 export default function ColorPicker({chosenColor,setChosenColor}) {
-    // const [chosenColor,setChosenColor] = useState('#fff')
-
     const handleColorChange = (color) => {
         setChosenColor(color)
     }
@@ -13,6 +11,7 @@ export default function ColorPicker({chosenColor,setChosenColor}) {
             <CirclePicker 
                 color={chosenColor}
                 onChange={handleColorChange}
+                colors={["#FFFF00","#FFFF33","#FF0000","#FD1C03","#00FF00","#00FF33","#00FFFF","#099FFF","#FF00FF","#FF00CC","#9D00FF","#CC00FF"]}
             />
         </>
     )
